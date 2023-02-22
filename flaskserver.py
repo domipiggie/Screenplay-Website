5ad67c712e440a94b22de8147512d49bca70f0c4
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-02-22 23:40:32 +0100

    keresés funkció

--- flaskserver.py
+++ flaskserver.py
@@ -8,21 +8,42 @@ CORS(app)
 
 screenplays = [
     {
-        'author': 'Jon Dough',
-        'title': 'Lorem`s ipsum',
+        'author': 'Scott Roberts based on screenplay by Patrick Meyers',
+        'title': 'K 2',
         'shortDesc': 'Very cool thing'
     },
     {
-        'author': 'Yu Ya',
-        'title': 'idk',
+        'author': 'Irene Mecchi & Jonathan Roberts',
+        'title': 'The Lion King',
         'shortDesc': 'Even cooler thing'
+    },
+    {
+        'author': 'Robert Riskin, based on the novel by James Hilton',
+        'title': 'Lost Horizon',
+        'shortDesc': 'omg cool thing'
     }
 ]
 
 
 @app.route("/")
-def main():
-    return render_template('index.html', screenplays=screenplays)
+def homePage():
+    return render_template('index.html')
+
+@app.route("/search", methods=["GET"])
+def searchPage():
+    toSearch = request.args.get('search', None)
+    
+    if (not toSearch):
+        print("eee")
+        return render_template('search.html')
+    
+    results = []
+    
+    for i in screenplays:
+        if i['title'].find(toSearch) != -1:
+            results.append(i)
+    
+    return render_template('search.html', results=results)
 
 
 app.run(host='0.0.0.0', port=8000, debug=True)
