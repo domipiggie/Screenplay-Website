d309399b23d81791d2c248d5d7b5fdd69849065b
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-02-22 17:47:28 +0100

    adatok átvitele szerverről kliensre

--- flaskserver.py
+++ flaskserver.py
@@ -1,23 +1,28 @@
 from datetime import date
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder="web")
 CORS(app)
 
-@app.get('/getTest')
-def hello_world():
-    return {
-        "greeting": ["hello", "world"],
-        "date": date.today()
+screenplays = [
+    {
+        'author': 'Jon Dough',
+        'title': 'Lorem`s ipsum',
+        'shortDesc': 'Very cool thing'
+    },
+    {
+        'author': 'Yu Ya',
+        'title': 'idk',
+        'shortDesc': 'Even cooler thing'
     }
+]
 
-@app.route('/', methods=['GET','POST'])
-def webhook():
-    if request.method == 'POST':
-        print("Data received is: ", request.json)
-        return {
-            "yes": ["yes", "yes"]
-        }
 
-app.run(host='0.0.0.0', port=8000)
+@app.route("/")
+def main():
+    return render_template('index.html', screenplays=screenplays)
+
+
+app.run(host='0.0.0.0', port=8000, debug=True)
