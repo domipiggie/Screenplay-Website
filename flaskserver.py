c7769ad7d79d2f27d4c682328f4f05e4bfcd6323
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-03-08 13:23:15 +0100

    search updates & pdf response
    Search is now based on an actual database instead of a dictionary hard-coded in the flaskserver.py file. Search results now have a clickable section that redirects the user to the correct pdf file.

--- flaskserver.py
+++ flaskserver.py
@@ -1,33 +1,25 @@
 from datetime import date
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 from flask_cors import CORS
+import sqlite3
 
-
-app = Flask(__name__, template_folder="web")
+DataBase = sqlite3.connect("screenData.db")
+DBcursor = DataBase.cursor()
+app = Flask(__name__, template_folder="")
 CORS(app)
 
-screenplays = [
-    {
-        'author': 'Scott Roberts based on screenplay by Patrick Meyers',
-        'title': 'K 2',
-        'shortDesc': 'Very cool thing'
-    },
-    {
-        'author': 'Irene Mecchi & Jonathan Roberts',
-        'title': 'The Lion King',
-        'shortDesc': 'Even cooler thing'
-    },
-    {
-        'author': 'Robert Riskin, based on the novel by James Hilton',
-        'title': 'Lost Horizon',
-        'shortDesc': 'omg cool thing'
-    }
-]
+screenplays = [a for a in DBcursor.execute("SELECT * FROM screenplays")]
 
 
 @app.route("/")
 def homePage():
-    return render_template('index.html')
+    return render_template('/web/index.html')
+
+@app.route("/screen", methods=["GET"])
+def screenPage():
+    response = make_response(open('./screenplays/'+request.args.get('screen', None), "rb"))
+    response.headers['Content-Type'] = 'application/pdf'
+    return response
 
 @app.route("/search", methods=["GET"])
 def searchPage():
@@ -35,15 +27,16 @@ def searchPage():
     
     if (not toSearch):
         print("eee")
-        return render_template('search.html')
+        return render_template('/web/search.html')
     
     results = []
     
     for i in screenplays:
-        if i['title'].find(toSearch) != -1:
+        print(i[1])
+        if i[1].lower().find(toSearch.lower()) != -1:
             results.append(i)
     
-    return render_template('search.html', results=results)
+    return render_template('/web/search.html', results=results)
 
 
 app.run(host='0.0.0.0', port=8000, debug=True)
