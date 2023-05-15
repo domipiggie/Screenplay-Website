243d3a87e789cabe71fbb45ea284d527c3ec9700
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2023-05-15 19:15:18 +0200

    keresés methódusokkal

--- flaskserver.py
+++ flaskserver.py
@@ -11,6 +11,31 @@ CORS(app)
 
 screenplays = [a for a in DBcursor.execute("SELECT * FROM screenplays ORDER BY screenplays.title")]
 
+def titleOrAuthContainSearch(screenplay, searchKey):
+    return screenplay[1].lower().find(searchKey.lower()) != -1 or screenplay[2].lower().find(searchKey.lower()) != -1
+
+def screenplayContainGenre(screenplay, genreList):
+    isCorrect = False
+            
+    j = 0
+    while not isCorrect and j < len(genreList):
+        if (screenplay[3].find(genreList[j]) != -1):
+            isCorrect = True
+        j+=1
+    
+    return isCorrect
+
+def screenplayContainType(screenplay, typeList):
+    isCorrect = False
+            
+    j = 0
+    while not isCorrect and j < len(typeList):
+        if (screenplay[4].find(typeList[j]) != -1):
+            isCorrect = True
+        j+=1
+    
+    return isCorrect
+    
 
 @app.route("/")
 def homePage():
@@ -31,8 +56,9 @@ def screenPage():
 def searchPage():
     toSearch = request.args.get('q', None)
     filters = request.args.get('f', None)
+    types = request.args.get('t', None)
     
-    if (not toSearch and not filters):
+    if (not toSearch and not filters and not types):
         return render_template('web/search.html', results=screenplays)
     
     if (toSearch == "bátya"):
@@ -40,29 +66,43 @@ def searchPage():
     
     results = []
     
-    if (not filters):
+    if (not filters and not types):
         for i in screenplays:
-            if i[1].lower().find(toSearch.lower()) != -1 or i[2].lower().find(toSearch.lower()) != -1:
+            if (titleOrAuthContainSearch(i, toSearch)):
                 results.append(i)
         
         return render_template('web/search.html', results=results)
     
+    if (not types):
+        filters = filters.split(",")
+        print(filters)
+        
+        for i in screenplays:
+            if (titleOrAuthContainSearch(i, toSearch) and screenplayContainGenre(i, filters)):
+                results.append(i)
+            
+        return render_template('web/search.html', results=results)
+    
+    if (not filters):
+        types = types.split(",")
+        print(types)
+        
+        for i in screenplays:
+            if (titleOrAuthContainSearch(i, toSearch) and screenplayContainType(i, types)):
+                results.append(i)
+            
+        return render_template('web/search.html', results=results)
+    
     filters = filters.split(",")
+    types = types.split(",")
+    print(filters, types)
     
     for i in screenplays:
-        isCorrect = False
-        
-        j = 0
-        while not isCorrect and j < len(filters):
-            if (i[3].find(filters[j]) != -1 or i[4].find(filters[j]) != -1):
-                isCorrect = True
-            j+=1
-        
-        if (isCorrect and (i[1].lower().find(toSearch.lower()) != -1 or i[2].lower().find(toSearch.lower()) != -1)):
+        if (titleOrAuthContainSearch(i, toSearch) and screenplayContainGenre(i, filters) and screenplayContainType(i, types)):
             results.append(i)
-                
     
     return render_template('web/search.html', results=results)
+
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
